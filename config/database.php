c50d1ff400982ba3d278023b2892e1e726e8c744
a20076288_Tiago_Silva <a20076288@alunos.ulht.pt>
2025-02-17 04:09:34 +0000

    v10

--- config/database.php
+++ config/database.php
@@ -37,19 +37,21 @@ return [
 
         // ✅ MYSQL — Forçado para Railway
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', getenv('MYSQLHOST') ?: '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'railway'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
+    'driver' => 'mysql',
+    'url' => env('DATABASE_URL'),
+    'host' => env('DB_HOST', getenv('MYSQLHOST') ?: '127.0.0.1'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE', getenv('MYSQLDATABASE') ?: 'railway'),
+    'username' => env('DB_USERNAME', getenv('MYSQLUSER') ?: 'root'),
+    'password' => env('DB_PASSWORD', getenv('MYSQLPASSWORD') ?: ''),
+    'unix_socket' => env('DB_SOCKET', ''),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => true,
+    'engine' => null,
+],
+
 
         // MARIA DB
         'mariadb' => [
