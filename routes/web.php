2a26aafeb24d21d5253f6d1f66fcf3b622afcfcd
a20076288_Tiago_Silva <a20076288@alunos.ulht.pt>
2025-02-17 02:55:39 +0000

    v3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/db-info', function () {
+    try {
+        $connection = DB::connection();
+        $database = $connection->getDatabaseName();
+        return "Laravel está ligado à base de dados: {$database}";
+    } catch (\Exception $e) {
+        return "Erro de conexão: " . $e->getMessage();
+    }
 });
